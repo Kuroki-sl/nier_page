2cd44c76099630da6569b547518ac3c07342fa70
Kuroki-sl <kuroki21189@gmail.com>
2026-02-07 14:05:58 -0300

    actu de textos

--- js/data/games.js
+++ js/data/games.js
@@ -1,7 +1,7 @@
 export const gamesDB = {
     favoritos: [
-        { id: 'automata', name: 'NieR: Automata', desc: 'Acción RPG sobre androides luchando por la humanidad.' },
-        { id: 'outerwilds', name: 'Outer Wilds', desc: 'Acción RPG sobre androides luchando por la humanidad.' },
+        { id: 'automata', name: 'NieR: Automata', desc: '¿Qué sucede cuando pierdes todo lo que te daba sentido? NieR: Automata utiliza la filosofía de Nietzsche y Sartre para contar una historia sobre el sufrimiento y la búsqueda de propósito. A través de los ojos de androides atrapados en una espiral de vida y muerte, descubrimos que el sentido de la existencia no se encuentra, se crea.' },
+        { id: 'outerwilds', name: 'Outer Wilds', desc: 'Si hay un aspecto que destaca de la comunidad de fans de este juego es que como si de un «club de la lucha» se tratase casi parece que tienen prohibido contar absolutamente nada a quienes no lo hayan jugado, algo que hasta al propio creador original del juego, Alex Beachum, le parece exagerado. Personalmente lo jugué sabiendo lo que me esperaba vagamente, apenas nada sobre su argumento o mecánicas y tras haberlo hecho así siento que, debo seguir la tradición de la primera regla de Outer Wilds y pedirte que, si lo que has leído ahora ha despertado tu curiosidad, lo juegues, no te pido que pagues por el si no te sientes comodo con ello, pero almenos y de manera fraudulenta, doy fe de que, como minimo, tendras una grata expeciencia.' },
         { id: 'celeste', name: 'Celeste', desc: 'Acción RPG sobre androides luchando por la humanidad.' },
         { id: 'replicant', name: 'NieR: Replicant', desc: 'Acción RPG sobre androides luchando por la humanidad.' }
     ],
